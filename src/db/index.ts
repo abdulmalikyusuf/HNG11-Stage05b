3adacf16d14179e5daea28d05cad9b2e8413664e
Abdulmalik Yusuf <31839174+abdulmalikyusuf@users.noreply.github.com>
2025-06-04 08:39:07 +0100

    fix: runtime error due to bcrypt in edge env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import type { Logger as drizzleLogger } from "drizzle-orm/logger";
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
-import bcrypt from "bcrypt";
 
 import * as schema from "./schema";
 import { env } from "@/env.mjs";
@@ -38,38 +37,3 @@ const db = drizzle({
   logger: new DBLogger(),
 });
 export { DB_ERRORS, poolConnection, db };
-
-const data = {
-  password: "password",
-  email: "example@mail.com",
-  firstName: "Example",
-  lastName: "DevLinks",
-} satisfies schema.InsertUser;
-
-export async function createNewProfile(info: schema.InsertUser = { ...data }) {
-  const saltRounds = 10;
-  const password = await bcrypt.hash(info.password, saltRounds);
-  try {
-    const profile = await db.insert(schema.users).values({ ...info, password });
-
-    const profileId = profile[0].insertId;
-
-    if (!profile) {
-      throw new Error(`Profile with Id ${profileId} not found.`);
-    }
-
-    console.log(`Password updated successfully for profile Id: ${profileId}`);
-    return profile;
-  } catch (error) {
-    console.error("Error updating the password:", error);
-    throw error;
-  }
-}
-// (async () => {
-//   try {
-//     await createNewProfile();
-//     console.log("Password updated successfully.");
-//   } catch (error) {
-//     console.error("Failed to update password:", error);
-//   }
-// })();
